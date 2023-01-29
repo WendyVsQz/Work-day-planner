76abc7a5347b3ca3bf5b6b317eb909c627e965ec
Wendy <118736912+WendyVsQz@users.noreply.github.com>
2023-01-29 19:57:22 +0000

    setting time colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,61 @@
 // calling targets:
 // 1. currentDate
 // 2. colorcode
-// 3. form control
-// 4. button
+// 3. button
 //--------------------------
-// Current time
+
+// *Current date
 
 var timeDisplayEl = document.querySelector('#currentDate');
 var currentDate = document.querySelector('answer');
 
 setInterval(function() {
     timeDisplayEl.textContent = moment().format("dddd, MMMM Do YYYY, h:mm:ss a")
-},1000)
-currentDateEl.textContent = moment()
+},1000);
 
-// Time coloring
+// *Colorcode
 
+// time matching colors
 
+var currentTime = moment().format('H');
+console.log()
 
-// button function
-
-
-
-
-// clear selector
-
+// make background color of / past hours gray / current hour red / future hours green
+const rows = document.getElementsByClassName("colorcode");
+var currentHour = parseInt(moment().format('H'));
 
 
+Array.from(rows).forEach(row => {
+    var
+    rowIdString = row.id,
+    rowHour;
+    if (rowIdString) {
+    rowHour = parseInt(rowIdString);
+    }
+    if (rowHour) {
+      // Compares row id to current hour and sets color accordingly
+    if (currentHour === rowHour) {
+        setColor(row, "#ca231a"); //present-red
+        } else if ((currentHour < rowHour) && (currentHour > rowHour - 12)) {
+        setColor(row, "#471b81"); //future-purple
+        } else if ((currentHour > rowHour) && (currentHour < rowHour + 12)) {
+        setColor(row, "#94a736"); //past-yellow brown
+        } else {
+        setColor(row, "#3e4647"); //Default
+        }
+    }
+});
 
-// saving to local storage
+function setColor(element, color) {
+    element.style.backgroundColor = color;
+}
+
+// *button function
+
+
+// A function you call whenever the menu button is clicked
+
+
+
+// *saving to local storage
+
